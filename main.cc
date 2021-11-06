774d3e9f15d89c698ea4f20a9d1cebe0250072d0
Fred Leitz <fred.leitz@gmail.com>
2021-11-05 20:16:54 -0700

    BPF linked

---
+++ main.cc
@@ -0,0 +1,16 @@
+#include <linux/version.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string>
+
+#include <bcc/BPF.h>
+
+
+int main(){
+    printf("BPF linked\n");
+    return 0;
+}
+
+
+
