643414f17db2f5b0ae03dbf5c210f391d6facd5e
Fred Leitz <fred.leitz@gmail.com>
2021-11-06 07:16:15 -0700

    finish notes

--- src/compile-tc.sh
+++ src/compile-tc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 clang -O2 -emit-llvm -I/usr/include -c tcbpf-sample.bpf.c -o - | llc-10 -march=bpf -filetype=obj -o tcbpf.o
 # remove old qdisc
+# change eno1 to whatever adapter, eg. eth0
 sudo tc qdisc del dev eno1 root handle 1
 
 sudo tc qdisc add dev eno1 root handle 1: prio 
